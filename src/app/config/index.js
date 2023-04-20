15bfebb7d651385ec2dee466e9e2728abb7aada5
Egu Chinedu <eguchinedu18@gmail.com>
2023-04-20 05:49:25 +0100

    hosted on firebase

---
+++ src/app/config/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqG2ljdK_7OmXTEm2Vf7UFdNUXJQ7iP4M",
+  authDomain: "angular-git-fetch.firebaseapp.com",
+  projectId: "angular-git-fetch",
+  storageBucket: "angular-git-fetch.appspot.com",
+  messagingSenderId: "240276447482",
+  appId: "1:240276447482:web:8e6eef51d8c22bb2fc6943",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
